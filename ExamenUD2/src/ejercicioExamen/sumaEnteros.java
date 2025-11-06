1297b6adf1256be0a7f64de78be823b7c7fa0cd3
estudiante <estudiante@DAM1-87d292>
2025-11-06 11:40:56 +0100

    versión 2

--- ExamenUD2/src/ejercicioExamen/sumaEnteros.java
+++ ExamenUD2/src/ejercicioExamen/sumaEnteros.java
@@ -1,5 +1,20 @@
 package ejercicioExamen;
 
 public class sumaEnteros {
+	public static void main (String[] args) {
+		try {
+			int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+			int resultado = calcularSuma(numeros);
+			System.out.println("La suma total es: " + resultado);
+		}catch(Exception e) {
+		}
+	}	
+		public static int calcularSuma (int[] valores)throws Exception{
+			int suma = 0;
+			for (int i = 0; i <= valores.length; i++) {
+				suma += valores[i];
+			}
+			return suma;
+		}
+	}
 
-}
